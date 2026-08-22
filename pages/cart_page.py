beeebcfd18f6742fe8f4841876b3578c2a7e0be1
CODE-DENYA <bumingshisamurai@gmail.com>
2026-08-22 18:55:16 +0700

    feat: добавлен тест удаления товара из корзины

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -9,10 +9,16 @@ class CartPage(BasePage):
 
         self.cart_items: Locator = page.locator("#cart_info_table tbody tr")
         self.product_names: Locator = page.locator(".cart_description h4 a")
+        self.delete_btn: Locator = page.locator(".cart_quantity_delete").first
+        self.empty_cart_msg: Locator = page.locator("#empty_cart")
 
     @allure.step("Перейти на страницу корзины")
     def open_cart_page(self):
         self.navigate("/view_cart")
+
+    @allure.step("Удалить первый товар из корзины")
+    def delete_first_product(self):
+        self.delete_btn.click()
 
     @allure.step("Получить количество товаров в корзине")
     def get_cart_items_count(self) -> int:
@@ -21,3 +27,7 @@ class CartPage(BasePage):
     @allure.step("Получить названия товаров в корзине")
     def get_product_names(self) -> list[str]:
         return self.product_names.all_text_contents()
+
+    @allure.step("Проверить, отображается ли сообщение о пустой корзине")
+    def is_cart_empty(self) -> bool:
+        return self.empty_cart_msg.is_visible()
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -14,3 +14,18 @@ def test_add_product_to_cart(page, base_url):
 
     assert cart_page.get_cart_items_count() > 0
     assert len(cart_page.get_product_names()) > 0
+
+
+@allure.feature("Корзина")
+@allure.story("Удаление товара из корзины")
+def test_delete_product_from_cart(page, base_url):
+    products_page = ProductsPage(page, base_url)
+    cart_page = CartPage(page, base_url)
+
+    products_page.open_products_page()
+    products_page.add_first_product_to_cart()
+
+    cart_page.delete_first_product()
+
+    page.wait_for_timeout(1000)
+    assert cart_page.is_cart_empty() or cart_page.get_cart_items_count() == 0
