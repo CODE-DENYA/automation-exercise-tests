cd58f023b62600410c3d55608ab40ebbf9ff33b7
CODE-DENYA <bumingshisamurai@gmail.com>
2026-08-22 19:15:14 +0700

    feat: добавлено прикрепление скриншотов в Allure при падении тестов

--- conftest.py
+++ conftest.py
@@ -1,9 +1,29 @@
 import os
+import allure
 import pytest
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 @pytest.fixture(scope="session")
 def base_url():
     return os.getenv("BASE_URL", "https://automationexercise.com")
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+
+
+@pytest.fixture(autouse=True)
+def attach_screenshot_on_failure(request, page):
+    yield
+    if hasattr(request.node, "rep_call") and request.node.rep_call.failed:
+        allure.attach(
+            page.screenshot(full_page=True),
+            name="failure_screenshot",
+            attachment_type=allure.attachment_type.PNG,
+        )
